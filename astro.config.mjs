3df86569d6b85997e1a57dad3d2dc5d4206debde
lumakestuff <lukazbujes@gmail.com>
2026-05-06 12:49:13 -0400

    updating dependencies

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,12 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import cloudflare from '@astrojs/cloudflare';
 
-// https://astro.build/config
 export default defineConfig({
-  // Replace with your domain
   site: 'https://lumakestuff.dev',
-
-  adapter: cloudflare()
+  output: 'server',
+  adapter: cloudflare({
+    prerenderEnvironment: 'node', 
+    imageService: 'compile'
+  })
 });
